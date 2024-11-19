cff78c9774d7c17dcbd15f059c98e5a4258e95ba
PerlaRuano <perla11916@cccneb.edu>
2024-11-19 17:27:06 -0600

    Import router from path

--- personnal-website/src/main.js
+++ personnal-website/src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 import Vue from 'vue';
-import router from './router'; // Import the router
+import router from 'C:\Users\perla\OneDrive\Documents\GitHub\portfolio-website-perla-ruano\personnal-website\src\router.js'; // Import the router
 
 Vue.config.productionTip = false;
 
