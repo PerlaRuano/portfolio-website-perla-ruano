6d41a9a02536f62939a55684bdf6f7e8aaf8400d
PerlaRuano <perla11916@cccneb.edu>
2024-11-20 13:55:04 -0600

    Instal Name changes

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -4,15 +4,18 @@
   </div>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomePage/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HomePage
   }
 }
 </script>
